8e47bc75c77b3eea5d87e7b147f31e6c1426ea01
Devesh00001 <devesh.choudhary@credextechnology.com>
2023-01-18 13:08:07 +0530

    addding

--- thirdBranch.java
+++
@@ -1,5 +0,0 @@
-public class thirdBranch {
-    public static void main(String[] args){
-        System.out.println("my buddy is pushkar sir");
-    }
-}
--- thirdBranch1.java
+++ thirdBranch1.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class thirdBranch1 {
+public class thirdBranch1 {
 }
